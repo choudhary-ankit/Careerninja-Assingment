7efb308ffb94ac5d311d09aface00937dff2fa0f
choudhary-ankit <ankit_chy@outlook.com>
2020-11-18 19:52:30 +0530

    update

--- src/Component/Aboutgame/About.js
+++ src/Component/Aboutgame/About.js
@@ -17,13 +17,13 @@ export default class About extends Component {
                                 Tic-tac-toe is a simple, two-player game that, 
                                 if played optimally by both players, will always 
                                 result in a tie. The game is also called noughts 
-                                and crosses or Xs and Os.
+                                and crosses or Xs and Os
                             </article>
                             <article>
                                 Tic-tac-toe is a game that is traditionally played 
                                 by being drawn on paper, and it can be played on a 
                                 computer or on a variety of media. Other games, 
-                                such as Connect 4, are based on this classic.
+                                such as Connect 4, are based on this classic
                             </article>
                         </div>
                     </Card>
